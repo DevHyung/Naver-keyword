7e9502059d603c57f5489aaeb8f611a7252cf294
DevHyung <khuphj@gmail.com>
2018-11-04 14:46:50 +0900

    1차 완성

---
+++ main.py
@@ -0,0 +1,113 @@
+"""
+title           :main.py
+description     :This will convert graph (naver keyword result ) to text or csv file
+author          :DevHyung
+date            :2018.11.04
+version         :1.0.0
+usage           :python3 main.py
+python_version  :3.6
+required module :selenium+chromewebdriver, csv, bs4
+"""
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup
+import time
+from openpyxl import load_workbook, Workbook
+import os
+def save_excel(_FILENAME, _DATA, _HEADER):
+    if os.path.exists(_FILENAME):
+        if _DATA == None:
+            return None
+        book = load_workbook(_FILENAME)
+        sheet = book.active
+        for depth1List in _DATA:
+            sheet.append(depth1List)
+        book.save(_FILENAME)
+    else:  # 새로만드는건
+        if _HEADER == None:
+            print(">>> 헤더 리스트를 먼저 넣어주세요")
+            return None
+        book = Workbook()
+        sheet = book.active
+        sheet.title = 'result'
+        sheet.append(_HEADER)
+        sheet.column_dimensions['A'].width = 20
+        sheet.column_dimensions['B'].width = 20
+        sheet.column_dimensions['C'].width = 20
+        sheet.column_dimensions['D'].width = 20
+        book.save(_FILENAME)
+FILENAME = "keyword.xlsx"
+HEADER = ['키워드', 'PC', 'MOBILE']
+# init
+save_excel(FILENAME,None,HEADER)
+
+if __name__=="__main__":
+    NAVER_ID = input("Naver ID 입력 :: ")
+    NAVER_PW = input("Naver PW 입력 :: ")
+    print(">>> Made By Pakr HyungJune copyright @ DevHyung")
+    # Setting variable
+    print("_" * 30)
+    print(">>> Loding....")
+    dir = './chromedriver'  # Driver Path
+    driver = webdriver.Chrome(dir)
+    driver.maximize_window()
+    # Login start
+    driver.get("https://searchad.naver.com/")  # target page
+    driver.find_element_by_xpath('//*[@id="uid"]').send_keys(NAVER_ID)
+    driver.find_element_by_xpath('//*[@id="upw"]').send_keys(NAVER_PW)
+    driver.find_element_by_xpath('//*[@id="container"]/main/div/div[1]/home-login/div/fieldset/span/button').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('/html/body/my-app/wrap/welcome-beginner-layer-popup/div[2]/div[1]/a').click()
+    driver.find_element_by_xpath('//*[@id="container"]/my-screen/div/div[1]/div/my-screen-board/div/div[1]/ul/li[3]/a').click()
+    # 이부분을 수정 하면 됌 내꺼는 2
+    driver.switch_to.window(driver.window_handles[1])
+
+    f = open("keyword.txt", 'r', encoding='utf8')
+    keywordList = f.readlines()
+    print(">>> {} 개 키워드 검색 시작".format(len(keywordList)))
+    # if event evoke, parsing start
+    idx = 1
+    dataList = []
+    for keyword in keywordList:
+        k = keyword.strip()
+        driver.find_element_by_xpath('//*[@id="wrap"]/div/div/div[1]/div[1]/div/div/div/div[2]/div[1]/div[1]/div[2]/form/div[1]/div/div/textarea').clear()
+        driver.find_element_by_xpath('//*[@id="wrap"]/div/div/div[1]/div[1]/div/div/div/div[2]/div[1]/div[1]/div[2]/form/div[1]/div/div/textarea').send_keys(k)
+        driver.find_element_by_xpath('//*[@id="wrap"]/div/div/div[1]/div[1]/div/div/div/div[2]/div[1]/div[1]/div[2]/form/div[4]/div/div/ul/li/button').click()
+        print(">>> {}번째, {} 진행중..".format(idx,k))
+        idx += 1
+        loopIdx = 1
+        while True:
+            try:
+                isSearch = False
+                bs4 = BeautifulSoup(driver.page_source, "lxml")
+                tr = bs4.find('table',class_='table table-bordered').find_all('tr')
+                for tmp in tr[2:]:
+                    if k.replace(' ','') == tmp.find('span',class_='keyword').get_text().replace(' ','').strip():#같은경우
+                        try:
+                            pc = tmp.find_all('td',class_=' text-right txt-r')[0].get_text().replace(',','')
+                        except:
+                            pc = "ERROR"
+                        try:
+                            mobile = tmp.find_all('td', class_=' text-right txt-r')[1].get_text().replace(',','')
+                        except:
+                            mobile = "ERROR"
+                        dataList.append([k,pc,mobile])
+                        if len(dataList) > 5:
+                            save_excel(FILENAME, dataList, None)
+                            dataList.clear()
+                        print("\t>>> {} 저장완료 ..".format(k))
+                        isSearch = True
+                if isSearch:
+                    break
+                else:
+                    time.sleep(0.5)
+                    loopIdx += 1
+                    if loopIdx == 3:
+                        dataList.append([k, '확인 바람', '확인 바람'])
+                        print("\t>>> {} 확인필요 ..".format(k))
+                        break
+            except:
+                time.sleep(0.5)
+
+    save_excel(FILENAME, dataList, None)
